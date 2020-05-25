0204895f12b264b41ee34d29c51c1584b7117ad4
zeroryuki <zeroryuki@gmail.com>
2020-05-26 07:17:08 +0800

    Add notification features
    -play azan on selected time via -n/--negeri flag

--- mysolatcli/__main__.py
+++ mysolatcli/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import pyjq
 import json
+import subprocess,time
 from mysolatcli import SolatAPI
 from yaspin import yaspin
 from argparse import ArgumentParser
@@ -23,12 +24,14 @@ def format_value(val):
         return "\n".join([ f"{k}: {val[k]}" for k in val.keys()])
     return val
 
+def notify(waktu):
+       subprocess.call("""echo "ffplay ~/bin/azan.mp3" | at """ + waktu, shell=True)
+
 def get_zon(lokasi):
     sp.start()
     zon = api.get_zones()
     zone = pyjq.all(".results[]|select(.lokasi == $lokasi)", zon, vars={"lokasi": lokasi})
     if zone == []:
-       #print("Lokasi not in any zone. Please try with another location")
        sp.red.fail("✘")
        print("Lokasi not in any zone. Please try with another location")
        sys.exit()
@@ -40,14 +43,18 @@ def data_for_jadual(data,fields):
     return data_format
 
 def jadual_lokasi(args):
-    lok = get_zon(args.lokasi.title())
-    if args.fields:
-       fields = ["tarikh"] + args.fields or ["tarikh","subuh","zohor","asar","maghrib","isyak"]
 
+    lok = get_zon(args.lokasi.title())
     data = pyjq.all(".prayer_times[]|{tarikh:.date,subuh:.subuh,zohor:.zohor,asar:.asar,maghrib:.maghrib,isyak:.isyak}", api.get_week(lok)) if args.minggu else pyjq.one(".|[{tarikh:.prayer_times.date,subuh:.prayer_times.subuh,zohor:.prayer_times.zohor,asar:.prayer_times.asar, maghrib:.prayer_times.maghrib,isyak:.prayer_times.isyak}]", api.get_today(lok))
 
+    if args.fields:
+       fields = ["tarikh"] + args.fields or ["tarikh","subuh","zohor","asar","maghrib","isyak"]
     data_format = data_for_jadual(data,fields)
-    print(tabulate(data_format,fields,tablefmt="fancy_grid"))
+    if args.notify:
+       for waktu in args.notify:
+          notify(data[0][waktu])
+    else:
+       print(tabulate(data_format,fields,tablefmt="fancy_grid"))
 
 def info_zon(args, fields=["zone","negeri","lokasi"]):
 
@@ -60,8 +67,7 @@ def info_zon(args, fields=["zone","negeri","lokasi"]):
         sp.start()
         for i in range(len(states)):
             fetch_zon = api.get_negeri(str(states[i]))
-            data = pyjq.all(".results[]", fetch_zon)
-            myzone.append(data)
+            myzone.append(pyjq.all(".results[]", fetch_zon))
             sp.hide()
             sp.write(states[i] + "✅")
         sp.ok()
@@ -75,7 +81,14 @@ def info_zon(args, fields=["zone","negeri","lokasi"]):
     else:
         sp.start()
         fetch_zon = api.get_today(args.zonkod)
-        data = pyjq.one(".|{zone,tarikh:.prayer_times.date,locations,azan:{subuh:.prayer_times.subuh,zohor: .prayer_times.zohor, asar: .prayer_times.asar, maghrib:.prayer_times.maghrib, isyak:.prayer_times.isyak}}",fetch_zon)
+        data = pyjq.one("""
+        .|{zone,tarikh:.prayer_times.date,locations,azan:{
+         subuh: .prayer_times.subuh,
+         zohor: .prayer_times.zohor,
+         asar:  .prayer_times.asar,
+         maghrib:.prayer_times.maghrib,
+         isyak: .prayer_times.isyak}}
+        """,fetch_zon)
         fields = data.keys()
         vals = list(map(lambda x: format_value(data[x]), fields))
         items = list(zip(fields, vals))
@@ -100,7 +113,7 @@ def parse_args():
     parser = ArgumentParser(
         prog="mysolatcli",
         description="Simple CLI tools for Malaysia Prayer Time"
-        
+
     )
     parser.set_defaults(command=None)
     command_parsers = parser.add_subparsers(title="commands", prog="mysolatcli")
@@ -109,6 +122,7 @@ def parse_args():
     jadual_parser.add_argument("-l","--lokasi", required=True, metavar="lokasi",  type=str, help="Show table based on location (Ex: gombak)")
     jadual_parser.add_argument("-m","--minggu", action="store_true", help="Print out prayer time for week")
     jadual_parser.add_argument("-f", "--fields", metavar="field", default=["subuh","zohor","asar","maghrib","isyak"], nargs="+", type=str, help="only print this value (Ex: zohor isyak)")
+    jadual_parser.add_argument("-n", "--notify", metavar="waktu", nargs="+", type=str, help="play azan on prayer time (Ex: zohor isyak)")
     jadual_parser.set_defaults(func=jadual_lokasi, command="jadual")
 
     zon_parser = command_parsers.add_parser("zon", help="Info for zones")
